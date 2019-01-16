8e8693567b1b67e2d6aee57e3168441034958cce
damakabdallah <xttrzax@gmail.com>
2019-01-16 19:54:05 +0100

    Initial commit

---
+++ Assets/scripts/CreateEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEffect : MonoBehaviour {
+
+    public GameObject GroundPound;
+    public GameObject explosion;
+    public GameObject RockChunk;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(GroundPound, this.transform);
+        Instantiate(explosion, this.transform);
+        Instantiate(RockChunk, new Vector3(transform.position.x,transform.position.y+2,transform.position.z),RockChunk.transform.rotation);
+
+    }
+}
---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float PSTimer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PSTimer -= Time.deltaTime;
+        if (PSTimer <= 0)
+            Destroy(gameObject);
+	}
+}
